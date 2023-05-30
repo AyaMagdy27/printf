8327746bdb2a9efc27d4ddde11832d77a4f49376
AyaMagdy27 <129787600+AyaMagdy27@users.noreply.github.com>
2023-05-30 14:26:24 +0300

    Update main

--- main.h
+++ main.h
@@ -66,6 +66,18 @@ typedef struct specifier
 int _puts(char *str);
 int _putchar(int c);
 
+/* print_functions.c module */
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_percent(va_list ap, params_t *params);
+int print_s(va_list ap, params_t *params);
+
+/* number.c module */
+char *convert(long int num, int base, int flags, params_t *params);
+int print_unsigned(va_list ap, params_t *params);
+int print_address(va_list ap, params_t *params);
+
 /* specifier.c module */
 int (*get_specifier(char *s))(va_list ap, params_t *params);
 int get_print_func(char *s, va_list ap, params_t *params);
@@ -73,10 +85,23 @@ int get_flag(char *s, params_t *params);
 int get_modeifier(char *s, params_t *params);
 int *get_width(char *s, params_t *params, va_list ap);
 
+/* convert_number.c module */
+int print_hex(va_list ap, params_t *params);
+int print_HEX(va_list ap, params_t *params);
+int print_binary(va_list ap, params_t *params);
+int print_octal(va_list ap, params_t *params);
+
 /* simple_printers.c module */
 int print_from_to(char *start, char *stop, char *except);
 int print_rev(va_list ap, params_t *params);
 int print_rot13(va_list ap, params_t *params);
+
+/* print_number.c module */
+int _isdigit(int c);
+int strlen(char *s);
+int print_number(char *str, params_t *params);
+int print_number_right_shift(char *str, params_t *params);
+int print_number_left_shift(char *str, params_t *params);
 
 /* params.c module */
 void init_params(params_t *params, va_list ap);
